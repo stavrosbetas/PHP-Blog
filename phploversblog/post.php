c8c21b0ecd31c7f3f95d8f7dd00df47f56b29264
stavrosbetas <wertstavros@hotmail.com>
2017-03-31 17:55:49 +0300

    show full text on post page

--- phploversblog/post.php
+++ phploversblog/post.php
@@ -23,7 +23,7 @@ $id = $_GET['id'];
             <h2 class="blog-post-title"><?php echo $post['title']; ?></h2>
             <p class="blog-post-meta"><?php echo formatDate($post['date']); ?> by :  <a href="#"><?php echo $post['author']; ?></a></p>
 
-            <p><?php echo shortenText($post['body']); ?></p>
+            <p><?php echo $post['body']; ?></p>
 
 
           </div><!-- /.blog-post -->
