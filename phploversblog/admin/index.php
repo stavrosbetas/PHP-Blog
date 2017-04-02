390b27d14d359276f3099609527fe6be284b9ae6
stavrosbetas <wertstavros@hotmail.com>
2017-04-02 21:49:38 +0300

    index.php for the admin area

---
+++ phploversblog/admin/index.php
@@ -0,0 +1,36 @@
+<?php include 'includes/header.php'?>
+
+
+
+		<table class="table table-striped">
+			<tr>
+				<th>Post ID#</th>
+				<th>Post Title</th>
+				<th>Category</th>
+				<th>Author</th>
+				<th>Date</th>
+			</tr>
+
+			<tr>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+		</table>
+		<table class="table table-striped">
+			<tr>
+				<th>Category ID#</th>
+				<th>category Name</th>
+
+			</tr>
+
+			<tr>
+				<td></td>
+				<td></td>
+			</tr>
+		</table>
+
+
+<?php include 'includes/footer.php'?>
