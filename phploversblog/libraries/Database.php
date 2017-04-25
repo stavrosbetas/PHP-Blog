bffd49ba256cc7af4c94a438c4c8854c7744d202
stavrosbetas <wertstavros@hotmail.com>
2017-04-25 15:10:57 +0300

    changes to Database.php

--- phploversblog/libraries/Database.php
+++ phploversblog/libraries/Database.php
@@ -4,35 +4,35 @@
 		public $username = DB_USER;
 		public $password = DB_PASS;
 		public $db_name = DB_NAME;
-		
+
 		public $link;
 		public $error;
-		
-		
+
+
 		/*
 		*	Class Constructor
 		*/
-		
-		
+
+
 		public function __construct(){
 
 			// Call Connect Function
 			$this->connect();
 		}
-		
+
 		/*
 		* Connector
 		*/
-		
+
 		private function connect(){
 			$this->link = new mysqli($this->host, $this->username, $this->password, $this->db_name);
-			
+
 			if(!$this->link){
 				$this->error = 'Connection Failed: ' . $this->link->connect_error;
 				return false;
 			}
 		}
-		
+
 		/*
 		* Select Method  select() function fetch all the data from users table.
 		*/
@@ -40,18 +40,18 @@
 			$result = $this->link->query($query) or die($this->link->error.__LINE__);
 			if($result->num_rows > 0){
 				return $result;
-				
+
 			}else
 				return false;
 		}
-		
+
 		/*
 		* Insert  insert() function have some parameters like $fname , $lname and $city which accepts input values from html form.
 		*/
-		
+
 		public function insert($query){
-			$insert_row -> $this->link->query($query) or die($this->link->error.__LINE__);
-			
+			$insert_row = $this->link->query($query) or die($this->link->error.__LINE__);
+
 			// Validate insert
 			if($insert_row){
 				header("Location:index.php?msg=".urlencode('Record Added'));
@@ -59,19 +59,19 @@
 			}else{
 				die('Error : ('. $this->link->erno . ') ' . $this->link->error);
 			}
-			 
-			
+
+
 		}
-		
-		
+
+
 		/*
 		* Update   update() : this function have four parameters with table name and table fields value.
 
 		*/
-		
+
 		public function update($query){
-			$update_row -> $this->link->query($query) or die($this->link->error.__LINE__);
-			
+			$update_row = $this->link->query($query) or die($this->link->error.__LINE__);
+
 			// Validate insert
 			if($update_row){
 				header("Location:index.php?msg=".urlencode('Record Updated'));
@@ -79,17 +79,17 @@
 			}else{
 				die('Error : ('. $this->link->erno . ') ' . $this->link->error);
 			}
-			 
-			
+
+
 		}
-		
+
 		/*
 		* Delete  delete() : function with $table and $id which is for MySql Delete Query.
 		*/
-		
+
 		public function delete($query){
-			$delete_row -> $this->link->query($query) or die($this->link->error.__LINE__);
-			
+			$delete_row = $this->link->query($query) or die($this->link->error.__LINE__);
+
 			// Validate insert
 			if($delete_row){
 				header("Location:index.php?msg=".urlencode('Record Updated'));
@@ -97,17 +97,17 @@
 			}else{
 				die('Error : ('. $this->link->erno . ') ' . $this->link->error);
 			}
-			 
-			
+
+
 		}
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
+
+
+
+
+
+
+
+
+
 }
